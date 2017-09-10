86bade7ebb26e8f116654b55d5251f96abbf0de6
yeissonleandrocruzrangel <playgaeton@gmail.com>
2017-09-10 08:57:49 -0500

    Se importaron todos los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,15 @@
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {  
+  MdAutocompleteModule,  MdButtonModule,  MdButtonToggleModule,  MdCardModule,
+  MdCheckboxModule,  MdChipsModule,  MdCoreModule,  MdDatepickerModule,  MdDialogModule,
+  MdExpansionModule,  MdGridListModule,  MdIconModule,  MdInputModule,  MdListModule,
+  MdMenuModule,  MdNativeDateModule,  MdPaginatorModule,  MdProgressBarModule,
+  MdProgressSpinnerModule,  MdRadioModule,  MdRippleModule,    MdSelectModule,  
+  MdSidenavModule,  MdSliderModule,  MdSlideToggleModule,  MdSnackBarModule,  MdSortModule,
+  MdTableModule,  MdTabsModule,  MdToolbarModule,  MdTooltipModule,
+  } from
+  '@angular/material';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {RouterModule, Routes} from '@angular/router';
 import {IndexComponent} from './customers/index/index.component';
 import {AddComponent as orderAddComponent} from './orders/add/add.component'
